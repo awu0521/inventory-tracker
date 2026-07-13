5214f9dd8e81c38d91b7f54241178e66f12e8736
Rafee1012 <rafee.ahsan5@gmail.com>
2026-07-12 18:41:49 -0700

    Added tests for itemComponentParser

--- tests/fixtures/ItemContainers.ts
+++ tests/fixtures/ItemContainers.ts
@@ -1,8 +1,20 @@
-import { testItem2 } from "./items";
+import { testItem1, testItem2 } from "./items";
 
-export const testContainer = {
+export const testContainer1 = {
     name: 'testContainer',
     contents: [testItem2],
+    weight: 20,
+    type: 'fragile',
+    dimensions: {
+        length: 20,
+        width: 15,
+        height: 8},
+    desc: 'testDesc2'
+    };
+
+    export const testContainer2 = {
+    name: 'testContainer',
+    contents: [testItem1, testItem2],
     weight: 20,
     type: 'fragile',
     dimensions: {
